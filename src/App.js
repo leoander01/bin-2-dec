d6e0f4e2a979fa63b3737c2ab4668435b8d79b23
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2021-05-03 22:51:53 -0300

    bin-2-dec

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+
+import Display from './components/Display';
+import Input from './components/Input';
+
+import './App.css';
+
+function App() {
+  const [ inputBin, setInputBin ] = useState({
+    value: '',
+    binary: false,
+  });
+
+  const [decNum, setDecNum] = useState();
+
+  useEffect(() => {
+    if (inputBin.value.length > 0 && inputBin.binary) {
+      setDecNum(convertToDec(inputBin.value));
+    } else if (inputBin.value.length === 0) {
+      setDecNum('');
+    }
+  }, [inputBin]);
+
+  const regExpNotBin = /[^01]/;
+
+  const handleBinaryInput = (e) => {
+    const { value } = e.target;
+    setInputBin({ value, binary: !regExpNotBin.test(value) })
+  }
+
+  const convertToDec = (num) => {
+    let convertedNum = 0;
+    num
+      .split('')
+      .reverse()
+      .map((item, index) => {
+        return item === '1' && (convertedNum += Math.pow(2, index))
+      })
+    return convertedNum
+  }
+
+  const Alert = () => {
+    if (inputBin.value.length > 0) {
+      if (!inputBin.binary) {
+        return (
+          <span style={{ color: '#e74c3c', fontWeight: 'bold' }}>
+            Você inseriu um dígito não binário (insira apenas 0 ou 1).
+          </span>
+        )
+      } else if (inputBin.binary) {
+        <span style={{ color: '#27ae60', fontWeight: 'bold' }}>
+          Aqui está o seu decimal!
+        </span>
+      } else {
+        return ' ';
+      }
+    }
+    return null;
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <span className="attenuated"></span>
+        <h1>Bin2Dec</h1>
+        <span className="attenuated"></span>
+      </div>
+
+      <p>
+        Insira um{' '}
+        <a
+          href="https://en.wikipedia.org/wiki/Binary_number"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          número binário
+        </a>
+        , obtenha uma{' '}
+        <a
+          href="https://en.wikipedia.org/wiki/Decimal"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          conversão decimal.
+        </a>{' '}
+
+        <br/>
+
+        <Alert />
+      </p>
+
+      <Input inputBin={inputBin} handleBinaryInput={handleBinaryInput} />
+      <Display inputBin={inputBin} decNum={decNum} />
+
+      <footer>
+        Feito por{' '}
+        <a
+          href="http://leoanderneves.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leoander Neves
+        </a>
+
+        <br/>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
